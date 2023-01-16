23dd01a1c1cbfe892cae3eb19e3df75d2307057e
OMEN <ayxannematov96@gmail.com>
2023-01-16 17:10:47 +0400

    frontend is ready to use

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://image-uploader-api.onrender.com/api/image",
+    baseUrl: "https://image-uploader-api-4ctd.onrender.com/api/image",
   }),
   tagTypes: ["Img"],
   endpoints: (builder) => ({}),
